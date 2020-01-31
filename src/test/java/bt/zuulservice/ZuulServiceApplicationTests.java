7b70b8af06cc86abc181fb16ad5eb58e0d4034db
prashdeep <pradeep.kumar44@gmail.com>
2020-01-31 16:37:03 +0530

    committing the zuul api

---
+++ src/test/java/bt/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bt.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
